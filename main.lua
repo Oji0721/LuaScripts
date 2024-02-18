13e280b99a96c49ca54ac152db6e89ebebe3bf22
Oji0721 <90021248+Oji0721@users.noreply.github.com>
2024-02-18 10:17:46 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,13 @@
-if game.placeId = 7305309231 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Oji0721/LuaScripts/main/taxiboss.lua"))() 
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Thank you for using our script!", -- Required
+	Text = "We are not responsible what happens in your account.", -- Required
+})
+wait(2)
+if game.placeId == 7305309231 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Oji0721/LuaScripts/main/taxiboss.lua"))()
+else
+  game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Error", -- Required
+	Text = "We don't have script for this game", -- Required
+})
 end
